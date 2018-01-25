6f1e76190c0180295d1052697864290919d81770
bdurb <brian@rpmgreenville.com>
2018-01-24 19:07:19 -0500

    imported Todos and set up app

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Todos from './Todos';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +8,31 @@ class App extends Component {
     this.state = {
       todo: '',
       todos: [],
-    }
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
+
+  onChange = (e) => {
+    this.setState({ todo: e.target.value });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      todo: '',
+      todos: [...this.state.todos, this.state.todo]
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>To Do List!</h1>
+        <form onSubmit={ this.onSubmit }>
+          <input value={ this.state.todo } onChange={ this.onChange } />
+          <button>Add It!</button>
+        </form>
+        <List todos={this.state.todos} />
       </div>
     );
   }
