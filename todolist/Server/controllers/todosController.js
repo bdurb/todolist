8510b2033af8d9b6d215c9f03e66399a3e99620f
bdurb <brian@rpmgreenville.com>
2018-01-29 17:48:17 -0500

    added controllers for database

---
+++ todolist/Server/controllers/todosController.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const Todo = mongoose.model('Todo');
+
+const STATUS_USER_ERROR = 422;
+
+const createTodo = (req, res) => {
+  const {todo} = req.query;
+  const newTodo = new Todo({ todo });
+  newPost.save()
+    .then((newPost) => {
+      res.json(newTodo);
+    })
+    .catch((err) => {
+      res.status(STATUS_USER_ERROR);
+      res.json(err);
+    })
+};
+
+const listTodos = (req, res) => {
+  Todo.find({})
+    .populate('todo')
+    .exec()
+    .then((todo) => {
+      res.json(todos);
+    })
+    .catch((err) => {
+      res.status(STATUS_USER_ERROR);
+      res.json({ stack: err.stack, message: err.message});
+    });
+};
+
+module.exports = {
+  createTodo,
+  listTodo,
+};
